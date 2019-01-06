82e90d54a7730c63ea6f4586ec489934776b4b3f
rov <valadares.thel@gmail.com>
2019-01-06 21:37:17 -0200

    Creates testes for model images parse

--- NetworkPlataformTests/APIConfigurationNetworkModel+Images+Specs.swift
+++ NetworkPlataformTests/APIConfigurationNetworkModel+Images+Specs.swift
@@ -7,12 +7,11 @@ import SwiftyJSON
 
 final class APIConfigurationNetworkModelImagesSpecs: QuickSpec {
     override func spec() {
-        
         describe("APIConfigurationNetworkModel.Images") {
             context("init") {
                 let imagesJSONStringFactory = ImagesJSONStringFactory()
                 
-                context("when input json is valid") {
+                context("when json is valid") {
                     let jsonString = imagesJSONStringFactory.make(with: .valid)
                     let json = JSON(parseJSON: jsonString)
                     
@@ -20,7 +19,71 @@ final class APIConfigurationNetworkModelImagesSpecs: QuickSpec {
                         expect { try APIConfigurationNetworkModel.Images(json: json) }.toNot(throwError())
                     }
                 }
-            }
+                
+                context("when json is missing key: secure_base_url") {
+                    let jsonString = imagesJSONStringFactory.make(with: .missingKeySecureBaseUrl)
+                    let json = JSON(parseJSON: jsonString)
+                    
+                    it("should throw error: ModelError.jsonWithMissingKey") {
+                        expect { try APIConfigurationNetworkModel.Images(json: json) }
+                            .to(throwError { (error: ModelError) in
+                                if case .jsonWithMissingKey(let key) = error {
+                                    expect(key) == "secure_base_url"
+                                } else {
+                                    fail()
+                                }
+                            })
+                    }
+                }
+                
+                context("when json is missing key: backdrop_sizes") {
+                    let jsonString = imagesJSONStringFactory.make(with: .missingKeyBackDropSizes)
+                    let json = JSON(parseJSON: jsonString)
+                    
+                    it("should throw error: ModelError.jsonWithMissingKey") {
+                        expect { try APIConfigurationNetworkModel.Images(json: json) }
+                            .to(throwError { (error: ModelError) in
+                                if case .jsonWithMissingKey(let key) = error {
+                                    expect(key) == "backdrop_sizes"
+                                } else {
+                                    fail()
+                                }
+                            })
+                    }
+                }
+                
+                context("when json has invalid content for key: secure_base_url") {
+                    let jsonString = imagesJSONStringFactory.make(with: .invalidContentOnSecureBaseUrl)
+                    let json = JSON(parseJSON: jsonString)
+                    
+                    it("should throw error: ModelError.jsonWithMissingKey") {
+                        expect { try APIConfigurationNetworkModel.Images(json: json) }
+                            .to(throwError { (error: ModelError) in
+                                if case .jsonWithInvalidInput(let key) = error {
+                                    expect(key) == "secure_base_url"
+                                } else {
+                                    fail()
+                                }
+                            })
+                    }
+                }
+                
+                context("when json has invalid content for key: backdrop_sizes") {
+                    let jsonString = imagesJSONStringFactory.make(with: .invalidContentOnKeyBackDropSizes)
+                    let json = JSON(parseJSON: jsonString)
+                    
+                    it("should throw error: ModelError.jsonWithMissingKey") {
+                        expect { try APIConfigurationNetworkModel.Images(json: json) }
+                            .to(throwError { (error: ModelError) in
+                                if case .jsonWithInvalidInput(let key) = error {
+                                    expect(key) == "backdrop_sizes"
+                                } else {
+                                    fail()
+                                }
+                            })
+                    }
+                }
+            } // init
         }
     }
 }
@@ -30,15 +93,17 @@ private final class ImagesJSONStringFactory {
         case valid
         case missingKeySecureBaseUrl
         case missingKeyBackDropSizes
-        case missingAllKeys
+        case invalidContentOnSecureBaseUrl
+        case invalidContentOnKeyBackDropSizes
     }
     
     func make(with scenario: Scenario) -> String {
         switch scenario {
         case .valid: return valid
-            case .missingKeySecureBaseUrl: return missingKeySecureBaseUrl
-            case .missingKeyBackDropSizes: return missingKeyBackDropSizes
-            case .missingAllKeys: return missingAllKeys
+        case .missingKeySecureBaseUrl: return missingKeySecureBaseUrl
+        case .missingKeyBackDropSizes: return missingKeyBackDropSizes
+        case .invalidContentOnSecureBaseUrl: return invalidContentOnSecureBaseUrl
+            case .invalidContentOnKeyBackDropSizes: return invalidContentOnKeyBackDropSizes
             
         }
     }
@@ -78,10 +143,25 @@ private final class ImagesJSONStringFactory {
         """
     }
     
-    private var missingAllKeys: String {
+    private var invalidContentOnSecureBaseUrl: String {
         return  """
             {
-        
+                "secure_base_url": "",
+                "backdrop_sizes": [
+                    "w300",
+                    "w780",
+                    "w1280",
+                    "original"
+                ]
+            }
+        """
+    }
+    
+    private var invalidContentOnKeyBackDropSizes: String {
+        return  """
+            {
+                "secure_base_url": "https://image.tmdb.org/t/p/",
+                "backdrop_sizes": [t]
             }
         """
     }
