d5ca0f37ce6bc8f8d7e330d64c44031283840cc4
rov <valadares.thel@gmail.com>
2019-01-20 10:14:09 -0200

    Organizes files from dataPlataform

---
+++ DataPlataform/UserDefaults/UserDefaultsDataProvider.swift
@@ -0,0 +1,9 @@
+//
+//  UserDefaultsDataProvider.swift
+//  DataPlataform
+//
+//  Created by Raoni de Oliveira Valadares on 1/20/19.
+//  Copyright © 2019 solo. All rights reserved.
+//
+
+import Foundation
---
+++ DataPlataform/UserDefaults/UserDefaultsDataProviderImp.swift
@@ -0,0 +1,9 @@
+//
+//  UserDefaultsDataProviderImp.swift
+//  DataPlataform
+//
+//  Created by Raoni de Oliveira Valadares on 1/20/19.
+//  Copyright © 2019 solo. All rights reserved.
+//
+
+import Foundation
